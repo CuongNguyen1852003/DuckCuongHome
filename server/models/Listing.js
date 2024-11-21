02fd088286a7037c7626430cf962ea605d2731bc
CuongNguyen1852003 <nttc1852003@gmail.com>
2024-11-21 22:28:43 +0700

    fixed error with models Listing.js

--- server/models/Listing.js
+++ server/models/Listing.js
@@ -1,125 +1,83 @@
-const router = require("express").Router();
-const multer = require("multer");
+const mongoose = require("mongoose")
 
-const Listing = require("../models/Listing");
-const User = require("../models/User")
-
-/* Configuration Multer for File Upload */
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
+const ListingSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    streetAddress: {
+      type: String,
+      required: true,
+    },
+    aptSuite: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    province: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    guestCount: {
+      type: Number,
+      required: true,
+    },
+    bedroomCount: {
+      type: Number,
+      required: true,
+    },
+    bedCount: {
+      type: Number,
+      required: true,
+    },
+    bathroomCount: {
+      type: Number,
+      required: true,
+    },
+    amenities: {
+      type: Array,
+      default:[]
+    },
+    listingPhotoPaths: [{ type: String }], // Store photo URLs
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    highlight: {
+      type: String,
+      required: true
+    },
+    highlightDesc: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+    }
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
-  },
-});
+  { timestamps: true}
+)
 
-const upload = multer({ storage });
-
-/* CREATE LISTING */
-router.post("/create", upload.array("listingPhotos"), async (req, res) => {
-    try {
-      /* Take the information from the form */
-      const {
-        creator,
-        category,
-        type,
-        streetAddress,
-        aptSuite,
-        city,
-        province,
-        country,
-        guestCount,
-        bedroomCount,
-        bedCount,
-        bathroomCount,
-        amenities,
-        title,
-        description,
-        highlight,
-        highlightDesc,
-        price,
-      } = req.body;
-  
-      const listingPhotos = req.files
-  
-      if (!listingPhotos) {
-        return res.status(400).send("No file uploaded.")
-      }
-  
-      const listingPhotoPaths = listingPhotos.map((file) => file.path)
-  
-      const newListing = new Listing({
-        creator,
-        category,
-        type,
-        streetAddress,
-        aptSuite,
-        city,
-        province,
-        country,
-        guestCount,
-        bedroomCount,
-        bedCount,
-        bathroomCount,
-        amenities,
-        listingPhotoPaths,
-        title,
-        description,
-        highlight,
-        highlightDesc,
-        price,
-      })
-  
-      await newListing.save()
-  
-      res.status(200).json(newListing)
-    } catch (err) {
-      res.status(409).json({ message: "Fail to create Listing", error: err.message })
-      console.log(err)
-    }
-  });
-
-  /* GET lISTINGS BY CATEGORY */
-router.get("/", async (req, res) => {
-    const qCategory = req.query.category
-  
-    try {
-      let listings
-      if (qCategory) {
-        listings = await Listing.find({ category: qCategory }).populate("creator")
-      } else {
-        listings = await Listing.find().populate("creator")
-      }
-  
-      res.status(200).json(listings)
-    } catch (err) {
-      res.status(404).json({ message: "Fail to fetch listings", error: err.message })
-      console.log(err)
-    }
-  })
-
-  /* GET LISTINGS BY SEARCH */
-router.get("/search/:search", async (req, res) => {
-    const { search } = req.params
-  
-    try {
-      let listings = []
-  
-      if (search === "all") {
-        listings = await Listing.find().populate("creator")
-      } else {
-        listings = await Listing.find({
-          $or: [
-            { category: {$regex: search, $options: "i" } },
-            { title: {$regex: search, $options: "i" } },
-          ]
-        }).populate("creator")
-      }
-  
-      res.status(200).json(listings)
-    } catch (err) {
-      res.status(404).json({ message: "Fail to fetch listings", error: err.message })
-      console.log(err)
-    }
-  })
+const Listing = mongoose.model("Listing", ListingSchema )
+module.exports = Listing
