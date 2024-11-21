0b6ad0aae16666c1c53123c719f4834ec9a51449
CuongNguyen1852003 <nttc1852003@gmail.com>
2024-11-21 22:15:35 +0700

    Configuration multer listing.js

---
+++ server/models/Listing.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const multer = require("multer");
+
+const Listing = require("../models/Listing");
+const User = require("../models/User")
+
+/* Configuration Multer for File Upload */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use the original file name
+  },
+});
+
+const upload = multer({ storage });
